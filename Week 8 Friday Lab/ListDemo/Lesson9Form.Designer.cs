be890963f80dff146eca591bc2dcf231f238ff7d
abedirahan <abedirhan@hotmail.com>
2019-06-28 10:11:31 -0400

    Added initial UI for Buttons

---
+++ Week 8 Friday Lab/ListDemo/Lesson9Form.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace ListDemo
+{
+    partial class Lesson9Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSeven = new System.Windows.Forms.Button();
+            this.NumberButtonTableLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.btnEight = new System.Windows.Forms.Button();
+            this.btnNine = new System.Windows.Forms.Button();
+            this.btnFour = new System.Windows.Forms.Button();
+            this.btnFive = new System.Windows.Forms.Button();
+            this.btnSix = new System.Windows.Forms.Button();
+            this.btnOne = new System.Windows.Forms.Button();
+            this.btnTwo = new System.Windows.Forms.Button();
+            this.btnThree = new System.Windows.Forms.Button();
+            this.btnZero = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.NumberButtonTableLayout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSeven
+            // 
+            this.btnSeven.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSeven.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnSeven.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSeven.Location = new System.Drawing.Point(8, 7);
+            this.btnSeven.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnSeven.Name = "btnSeven";
+            this.btnSeven.Size = new System.Drawing.Size(241, 60);
+            this.btnSeven.TabIndex = 0;
+            this.btnSeven.Text = "7";
+            this.btnSeven.UseVisualStyleBackColor = false;
+            // 
+            // NumberButtonTableLayout
+            // 
+            this.NumberButtonTableLayout.ColumnCount = 3;
+            this.NumberButtonTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.NumberButtonTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.NumberButtonTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.NumberButtonTableLayout.Controls.Add(this.btnZero, 1, 3);
+            this.NumberButtonTableLayout.Controls.Add(this.btnThree, 2, 2);
+            this.NumberButtonTableLayout.Controls.Add(this.btnTwo, 1, 2);
+            this.NumberButtonTableLayout.Controls.Add(this.btnOne, 0, 2);
+            this.NumberButtonTableLayout.Controls.Add(this.btnSix, 2, 1);
+            this.NumberButtonTableLayout.Controls.Add(this.btnFive, 1, 1);
+            this.NumberButtonTableLayout.Controls.Add(this.btnFour, 0, 1);
+            this.NumberButtonTableLayout.Controls.Add(this.btnNine, 2, 0);
+            this.NumberButtonTableLayout.Controls.Add(this.btnEight, 1, 0);
+            this.NumberButtonTableLayout.Controls.Add(this.btnSeven, 0, 0);
+            this.NumberButtonTableLayout.Location = new System.Drawing.Point(12, 84);
+            this.NumberButtonTableLayout.Name = "NumberButtonTableLayout";
+            this.NumberButtonTableLayout.RowCount = 4;
+            this.NumberButtonTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.NumberButtonTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.NumberButtonTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.NumberButtonTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.NumberButtonTableLayout.Size = new System.Drawing.Size(774, 299);
+            this.NumberButtonTableLayout.TabIndex = 1;
+            // 
+            // btnEight
+            // 
+            this.btnEight.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEight.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnEight.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnEight.Location = new System.Drawing.Point(265, 7);
+            this.btnEight.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnEight.Name = "btnEight";
+            this.btnEight.Size = new System.Drawing.Size(241, 60);
+            this.btnEight.TabIndex = 1;
+            this.btnEight.Text = "8";
+            this.btnEight.UseVisualStyleBackColor = false;
+            // 
+            // btnNine
+            // 
+            this.btnNine.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNine.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnNine.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnNine.Location = new System.Drawing.Point(522, 7);
+            this.btnNine.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnNine.Name = "btnNine";
+            this.btnNine.Size = new System.Drawing.Size(244, 60);
+            this.btnNine.TabIndex = 2;
+            this.btnNine.Text = "9";
+            this.btnNine.UseVisualStyleBackColor = false;
+            // 
+            // btnFour
+            // 
+            this.btnFour.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFour.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnFour.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnFour.Location = new System.Drawing.Point(8, 81);
+            this.btnFour.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnFour.Name = "btnFour";
+            this.btnFour.Size = new System.Drawing.Size(241, 60);
+            this.btnFour.TabIndex = 3;
+            this.btnFour.Text = "4";
+            this.btnFour.UseVisualStyleBackColor = false;
+            // 
+            // btnFive
+            // 
+            this.btnFive.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFive.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnFive.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnFive.Location = new System.Drawing.Point(265, 81);
+            this.btnFive.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnFive.Name = "btnFive";
+            this.btnFive.Size = new System.Drawing.Size(241, 60);
+            this.btnFive.TabIndex = 4;
+            this.btnFive.Text = "5";
+            this.btnFive.UseVisualStyleBackColor = false;
+            // 
+            // btnSix
+            // 
+            this.btnSix.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSix.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnSix.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSix.Location = new System.Drawing.Point(522, 81);
+            this.btnSix.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnSix.Name = "btnSix";
+            this.btnSix.Size = new System.Drawing.Size(244, 60);
+            this.btnSix.TabIndex = 5;
+            this.btnSix.Text = "6";
+            this.btnSix.UseVisualStyleBackColor = false;
+            // 
+            // btnOne
+            // 
+            this.btnOne.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOne.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnOne.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnOne.Location = new System.Drawing.Point(8, 155);
+            this.btnOne.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnOne.Name = "btnOne";
+            this.btnOne.Size = new System.Drawing.Size(241, 60);
+            this.btnOne.TabIndex = 6;
+            this.btnOne.Text = "1";
+            this.btnOne.UseVisualStyleBackColor = false;
+            // 
+            // btnTwo
+            // 
+            this.btnTwo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnTwo.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnTwo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnTwo.Location = new System.Drawing.Point(265, 155);
+            this.btnTwo.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnTwo.Name = "btnTwo";
+            this.btnTwo.Size = new System.Drawing.Size(241, 60);
+            this.btnTwo.TabIndex = 7;
+            this.btnTwo.Text = "2";
+            this.btnTwo.UseVisualStyleBackColor = false;
+            // 
+            // btnThree
+            // 
+            this.btnThree.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnThree.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnThree.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnThree.Location = new System.Drawing.Point(522, 155);
+            this.btnThree.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnThree.Name = "btnThree";
+            this.btnThree.Size = new System.Drawing.Size(244, 60);
+            this.btnThree.TabIndex = 8;
+            this.btnThree.Text = "3";
+            this.btnThree.UseVisualStyleBackColor = false;
+            // 
+            // btnZero
+            // 
+            this.btnZero.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnZero.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnZero.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnZero.Location = new System.Drawing.Point(265, 229);
+            this.btnZero.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnZero.Name = "btnZero";
+            this.btnZero.Size = new System.Drawing.Size(241, 63);
+            this.btnZero.TabIndex = 10;
+            this.btnZero.Text = "0";
+            this.btnZero.UseVisualStyleBackColor = false;
+            // 
+            // lblResult
+            // 
+            this.lblResult.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblResult.Location = new System.Drawing.Point(13, 13);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(765, 55);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "0";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Lesson9Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.NumberButtonTableLayout);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "Lesson9Form";
+            this.Text = "Lesson 9";
+            this.NumberButtonTableLayout.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSeven;
+        private System.Windows.Forms.TableLayoutPanel NumberButtonTableLayout;
+        private System.Windows.Forms.Button btnZero;
+        private System.Windows.Forms.Button btnThree;
+        private System.Windows.Forms.Button btnTwo;
+        private System.Windows.Forms.Button btnOne;
+        private System.Windows.Forms.Button btnSix;
+        private System.Windows.Forms.Button btnFive;
+        private System.Windows.Forms.Button btnFour;
+        private System.Windows.Forms.Button btnNine;
+        private System.Windows.Forms.Button btnEight;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
